e75ea8142ea05db02bd47a211a310c56e91dfc0f
laurent bernabé <laurent.bernabe@gmail.com>
2026-01-09 11:44:17 +0100

    feat: initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/loloof64/color_sequence_memory/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.loloof64.color_sequence_memory
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
